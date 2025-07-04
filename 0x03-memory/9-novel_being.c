efe35429b02ffb329e9d7169dea7ce77da755135
manar <manarmohamed.icloud.com@gmail.com>
2025-07-04 21:03:00 +0300

    Task 9

---
+++ 0x03-memory/9-novel_being.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+struct Being
+{
+char *name;
+int age;
+float energy_evel;
+}
+void print_being(struct Being *b)
+{
+if (b==NULL)
+return;
+printf("name: %s, age:%d,energy : %.2f \n ",b->name, b->age, b->energy_level);
+}
+int main ()
+{
+struct Being alien ={"mamr",21 , 87.45}
+print_being(&alien);
+return 0;
+}
